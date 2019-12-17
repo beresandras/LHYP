c366ae7c093a25119aa891d8db8c759b872fcccd
beresandras <30826103+beresandras@users.noreply.github.com>
2019-12-17 03:13:06 +0100

    Több háló automatizált tanítása, eredmények kimentése kész

--- Trainer.py
+++ Trainer.py
@@ -1,4 +1,6 @@
 import os
+import time
+import copy
 from enum import Enum
 import numpy as np
 import PIL
@@ -44,9 +46,9 @@ class Pathology(Enum):
     FABRY         = 9
 
 class DatasetParameters:
-    def __init__(self, data_dir, results_dir, expected_pathologies):
+    def __init__(self, data_dir, dataset_path, expected_pathologies):
         self.data_dir = data_dir
-        self.results_dir = results_dir
+        self.dataset_path = dataset_path
         self.patient_ids = [pid for pid in sorted(os.listdir(data_dir)) if pid[0] is not '_']
         if len(self.patient_ids) == 0:
             print('Error: no data found in folder {}'.format(data_dir))
@@ -137,7 +139,7 @@ class DatasetParameters:
         np.random.shuffle(self.shuffled_indices_lale)
 
     def save_object(self):
-        with open(self.results_dir + '/_dataset_params.pickle', 'wb') as outfile:
+        with open(self.dataset_path, 'wb') as outfile:
             pickle.dump(self, outfile)
 
 class GenericHypertrophyDataset(Dataset):
@@ -146,7 +148,6 @@ class GenericHypertrophyDataset(Dataset):
         self.dataset_mode = dataset_mode
         self.medical_mode = medical_mode
         self.data_dir = dataset_params.data_dir
-        self.results_dir = dataset_params.results_dir
         self.augmenter = augmenter
 
         if self.medical_mode == MedicalMode['SA']:
@@ -216,50 +217,82 @@ class GenericHypertrophyDataset(Dataset):
     def __len__(self):
         return self.length
 
-if __name__=='__main__':
+def train_model(model, model_info, epochs, batch_size, gpu_index, seed):
+    start_time = time.time()
+    force_analyzis = False
+    show_image = False
 
     data_dir = '../data/beres'
     results_dir = '../results'
     models_dir = '../models'
-    force_analyzis = False
 
-    model_save_frequency = 5
-    model_path = models_dir + '/resnet18_{}_{}.pth'
-    show_image = False
+    dataset_str = '{}Class'.format(model_info['num_classes'])
+    model_str = '{}_{}_{}Class_{}Aug_{}'.format(model_info['name'], model_info['medical_mode'], model_info['num_classes'], \
+                                                model_info['augmentation'], model_info['transfer'])
+
+    dataset_path = results_dir + '/dataset_' + dataset_str + '.pickle'
+    model_path = models_dir + '/model_' + model_str + '.pth'
+    results_path = results_dir + '/result_' + model_str + '.pickle'
 
     if not torch.cuda.is_available():
         print('CUDA is not available')
         exit()
-    device = torch.device(1)
+    #torch.cuda.empty_cache()
+    #torch.cuda.ipc_collect()
+    device = torch.device(gpu_index)
 
-    seed = 12
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.seed_all()
 
-    pathology_to_label = {
-        Pathology['NORMAL']: 0,
-        Pathology['HCM']: 1,
-        Pathology['AMY']: 2,
-        Pathology['AMYLOIDOSIS']: 2,
-        Pathology['EMF']: 2,
-        Pathology['AORTASTENOSIS']: 2,
-        Pathology['FABRY']: 2
+    pathology_to_label_dict = {
+        2: {
+            Pathology['NORMAL']: 0,
+            Pathology['HCM']: 1,
+            Pathology['AMY']: 1,
+            Pathology['AMYLOIDOSIS']: 1,
+            Pathology['EMF']: 1,
+            Pathology['AORTASTENOSIS']: 1,
+            Pathology['FABRY']: 1
+        },
+        3: {
+            Pathology['NORMAL']: 0,
+            Pathology['HCM']: 1,
+            Pathology['AMY']: 2,
+            Pathology['AMYLOIDOSIS']: 2,
+            Pathology['EMF']: 2,
+            Pathology['AORTASTENOSIS']: 2,
+            Pathology['FABRY']: 2
+        },
+        4: {
+            Pathology['NORMAL']: 0,
+            Pathology['HCM']: 1,
+            Pathology['AMY']: 2,
+            Pathology['AMYLOIDOSIS']: 2,
+            Pathology['EMF']: 2,
+            Pathology['AORTASTENOSIS']: 2,
+            Pathology['FABRY']: 2,
+            Pathology['ADULT_M_SPORT']: 3,
+            Pathology['ADULT_F_SPORT']: 3,
+            Pathology['U18_F']: 3,
+            Pathology['U18_M']: 3,
+        }
     }
+    pathology_to_label = pathology_to_label_dict[model_info['num_classes']]
 
-    if (force_analyzis != True and os.path.isfile(results_dir + '/_dataset_params.pickle')):
-        with open(results_dir + '/_dataset_params.pickle', 'rb') as param_file:
+    if (force_analyzis != True and os.path.isfile(dataset_path)):
+        with open(dataset_path, 'rb') as param_file:
             dataset_params = pickle.load(param_file)
     else:
-        dataset_params = DatasetParameters(data_dir, results_dir, pathology_to_label)
+        dataset_params = DatasetParameters(data_dir, dataset_path, pathology_to_label)
         dataset_params.analyze_data()
         dataset_params.save_object()
-
-    tqdm.write('Number of all patients: {}'.format(len(dataset_params.patient_ids)))
-    tqdm.write('Number of SA images: {}, patients: {}'.format(len(dataset_params.patient_ids_sa), len(set(dataset_params.patient_ids_sa))))
-    tqdm.write('Number of SALE images: {}, patients: {}'.format(len(dataset_params.patient_ids_sale), len(set(dataset_params.patient_ids_sale))))
-    tqdm.write('Number of LA images: {}, patients: {}'.format(len(dataset_params.patient_ids_la), len(set(dataset_params.patient_ids_la))))
-    tqdm.write('Number of LALE images: {}, patients: {}'.format(len(dataset_params.patient_ids_lale), len(set(dataset_params.patient_ids_lale))))
+        tqdm.write('New dataset parameters have been created:')
+        tqdm.write('Number of all patients: {}'.format(len(dataset_params.patient_ids)))
+        tqdm.write('Number of SA images: {}, patients: {}'.format(len(dataset_params.patient_ids_sa), len(set(dataset_params.patient_ids_sa))))
+        tqdm.write('Number of SALE images: {}, patients: {}'.format(len(dataset_params.patient_ids_sale), len(set(dataset_params.patient_ids_sale))))
+        tqdm.write('Number of LA images: {}, patients: {}'.format(len(dataset_params.patient_ids_la), len(set(dataset_params.patient_ids_la))))
+        tqdm.write('Number of LALE images: {}, patients: {}'.format(len(dataset_params.patient_ids_lale), len(set(dataset_params.patient_ids_lale))))
 
     dataset_split = {
         DatasetMode['TRAIN']: [0.0, 0.7],
@@ -267,20 +300,54 @@ if __name__=='__main__':
         DatasetMode['TEST']: [0.85, 1.0]
     }
 
-    medical_mode_str = 'SALE'
-    medical_mode = MedicalMode[medical_mode_str]
-    epochs = 20
-    batch_size = 16
+    medical_mode = MedicalMode[model_info['medical_mode']]
 
-    model = models.resnet.resnet18(num_classes=3) #models.vgg.vgg11_bn(num_classes=3)
-    test_model = models.resnet.resnet18(num_classes=3)
     model_input_size = 224
-
     model.to(device)
-    test_model.to(device)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    augmentation_dict = {
+        'min': transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.RandomAffine((0, 0), translate=(0, 0), scale=(2, 2)),
+            transforms.CenterCrop((model_input_size, model_input_size)),
+            transforms.Grayscale(num_output_channels=3),
+            transforms.ToTensor()
+        ]),
+        'norm': transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.RandomAffine((-20, 20), translate=(0.1, 0.1), scale=(1.8, 2.2)),
+            transforms.CenterCrop((model_input_size, model_input_size)),
+            transforms.Grayscale(num_output_channels=3),
+            transforms.ToTensor()
+        ]),
+        'max': transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.RandomAffine((-30, 30), translate=(0.15, 0.15), scale=(1.7, 2.3)),
+            transforms.CenterCrop((model_input_size, model_input_size)),
+            transforms.Grayscale(num_output_channels=3),
+            transforms.ColorJitter(brightness=0.3),
+            transforms.ToTensor()
+        ]),
+        'min_transf': transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.RandomAffine((0, 0), translate=(0, 0), scale=(2, 2)),
+            transforms.CenterCrop((model_input_size, model_input_size)),
+            transforms.Grayscale(num_output_channels=3),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ]),
+        'norm_transf': transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.RandomAffine((-20, 20), translate=(0.1, 0.1), scale=(1.8, 2.2)),
+            transforms.CenterCrop((model_input_size, model_input_size)),
+            transforms.Grayscale(num_output_channels=3),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+    }
 
     train_loader = DataLoader(
         GenericHypertrophyDataset(
@@ -288,56 +355,43 @@ if __name__=='__main__':
             dataset_split,
             medical_mode,
             dataset_params,
-            transforms.Compose([
-                transforms.ToPILImage(),
-                transforms.RandomAffine((-20, 20), translate=(0.1, 0.1), scale=(1.8, 2.2)),
-                transforms.CenterCrop((model_input_size, model_input_size)),
-                transforms.Grayscale(num_output_channels=3),
-                transforms.ToTensor()
-            ])
+            (augmentation_dict[model_info['augmentation']] if model_info['transfer'] == 'retrain' else augmentation_dict[model_info['augmentation'] + '_transf'])
         ),
-        batch_size=batch_size
+        batch_size=batch_size,
+        drop_last=True
     )
 
     validation_loader = DataLoader(
         GenericHypertrophyDataset(
-            DatasetMode['TRAIN'],
+            DatasetMode['VALIDATION'],
             dataset_split,
             medical_mode,
             dataset_params,
-            transforms.Compose([
-                transforms.ToPILImage(),
-                transforms.RandomAffine((0, 0), translate=(0, 0), scale=(2, 2)),
-                transforms.CenterCrop((model_input_size, model_input_size)),
-                transforms.Grayscale(num_output_channels=3),
-                transforms.ToTensor()
-            ])
+            (augmentation_dict['min'] if model_info['transfer'] == 'retrain' else augmentation_dict['min_transf'])
         ),
-        batch_size=batch_size
+        batch_size=batch_size,
+        drop_last=True
     )
 
     test_loader = DataLoader(
         GenericHypertrophyDataset(
-            DatasetMode['TRAIN'],
+            DatasetMode['TEST'],
             dataset_split,
             medical_mode,
             dataset_params,
-            transforms.Compose([
-                transforms.ToPILImage(),
-                transforms.RandomAffine((0, 0), translate=(0, 0), scale=(2, 2)),
-                transforms.CenterCrop((model_input_size, model_input_size)),
-                transforms.Grayscale(num_output_channels=3),
-                transforms.ToTensor()
-            ])
+            (augmentation_dict['min'] if model_info['transfer'] == 'retrain' else augmentation_dict['min_transf'])
         ),
-        batch_size=batch_size
+        batch_size=batch_size,
+        drop_last=True
     )
 
-    tqdm.write('Training on {} for {} epochs'.format(medical_mode_str, epochs))
+    tqdm.write('\nTraining {} for {} epochs'.format(model_str, epochs))
+    model_info['num_trainable_params'] = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    min_validation_loss = None
     for epoch in range(epochs):
         avg_train_loss = 0.0
         avg_train_acc = 0
-        for batch_index, data_batch in tqdm(enumerate(train_loader), total=len(train_loader)):
+        for _, data_batch in tqdm(enumerate(train_loader), total=len(train_loader)):
             image_batch, label_batch = data_batch
             image_batch = image_batch.to(device)
             label_batch = label_batch.to(device)
@@ -360,7 +414,7 @@ if __name__=='__main__':
 
         avg_validation_loss = 0.0
         avg_validation_acc = 0
-        for batch_index, data_batch in enumerate(validation_loader):
+        for ba_tch_index, data_batch in enumerate(validation_loader):
             model.eval()
             image_batch, label_batch = data_batch
             image_batch = image_batch.to(device)
@@ -379,22 +433,22 @@ if __name__=='__main__':
         avg_train_acc = float(avg_train_acc) / (len(train_loader) * batch_size)
         avg_validation_acc = float(avg_validation_acc) / (len(validation_loader) * batch_size)
 
-        tqdm.write("epoch: {}, train_loss: {:.3f}, train_acc: {:.3f}, val_loss: {:.3f}, val_acc: {:.3f}"\
+        tqdm.write('epoch: {}, train_loss: {:.3f}, train_acc: {:.3f}, val_loss: {:.3f}, val_acc: {:.3f}'\
                 .format(epoch, avg_train_loss, avg_train_acc, avg_validation_loss, avg_validation_acc))
+        model_info['val_losses'].append(avg_validation_loss)
+        model_info['val_accs'].append(avg_validation_acc)
 
-        if (epoch + 1) % model_save_frequency == 0:
-            torch.save(model.state_dict(), model_path.format(medical_mode_str, epoch))
+        if min_validation_loss == None or min_validation_loss > avg_validation_loss:
+            min_validation_loss = avg_validation_loss
+            torch.save(model.state_dict(), model_path)
             tqdm.write('Model saved at epoch {}'.format(epoch))
-    if (epoch + 1) % model_save_frequency != 0:
-        torch.save(model.state_dict(), model_path.format(medical_mode_str, epoch))
-        tqdm.write('Model saved at epoch {}'.format(epoch))
     
-    load_epoch = input('Load model from epoch: ')
-    test_model.load_state_dict(torch.load(model_path.format(medical_mode_str, load_epoch)))
-
+    model.load_state_dict(torch.load(model_path))
+    
     avg_test_loss = 0.0
     avg_test_acc = 0
-    for batch_index, data_batch in enumerate(test_loader):
+    model_info['confusion_matrix'] = torch.zeros(model_info['num_classes'], model_info['num_classes'])
+    for _, data_batch in enumerate(test_loader):
         model.eval()
         image_batch, label_batch = data_batch
         image_batch = image_batch.to(device)
@@ -406,6 +460,144 @@ if __name__=='__main__':
 
         avg_test_loss += loss.item()
         avg_test_acc += torch.sum(predictions == label_batch)
+
+        for truth, prediction in zip(label_batch.view(-1), predictions.view(-1)):
+            model_info['confusion_matrix'][truth.long(), prediction.long()] += 1
+
     avg_test_loss /= len(test_loader)
     avg_test_acc = float(avg_test_acc) / (len(test_loader) * batch_size)
-    tqdm.write("test_loss: {:.3f}, test_acc: {:.3f}".format(avg_test_loss, avg_test_acc))
+    tqdm.write('test_loss: {:.3f}, test_acc: {:.3f}'.format(avg_test_loss, avg_test_acc))
+    model_info['test_loss'] = avg_test_loss
+    model_info['test_acc'] = avg_test_acc
+    model_info['train_time']= (time.time() - start_time) / (epochs * batch_size)
+
+    with open(results_path, 'wb') as out_file:
+        pickle.dump(model_infos, out_file)
+
+if __name__=='__main__':
+    epochs = 2
+    batch_size = 32
+
+    gpu_index = 1
+    seed = 10
+
+    model_objects = [
+        models.resnet.resnet18(num_classes=3),
+        models.resnet.resnet18(num_classes=3),
+        models.resnet.resnet18(num_classes=3),
+        models.resnet.resnet18(num_classes=3),
+        models.resnet.resnet18(num_classes=3),
+        models.resnet.resnet18(num_classes=3),
+        
+        models.vgg.vgg11_bn(num_classes=3),
+        models.squeezenet.squeezenet1_0(num_classes=3),
+        models.mobilenet.mobilenet_v2(num_classes=3),
+        
+        models.resnet.resnet18(num_classes=2),
+        models.resnet.resnet18(num_classes=4),
+        
+        models.resnet.resnet18(num_classes=3),
+        models.resnet.resnet18(num_classes=3),
+        
+        models.resnet.resnet18(pretrained=True),
+        models.resnet.resnet18(pretrained=True)
+    ]
+
+    # finetune: modify final layer
+    model_objects[-2].fc = nn.Linear(model_objects[-2].fc.in_features, 3)
+
+    # transfer-learn: modify final layer and freeze the others
+    for param in model_objects[-1].parameters():
+        param.requires_grad = False
+    model_objects[-1].fc = nn.Linear(model_objects[-2].fc.in_features, 3)
+
+    base_model_info = {
+        'name': 'ResNet',
+        'year': 2015,
+        'medical_mode': 'SA',
+        'num_classes': 3,
+        'augmentation': 'norm',
+        'transfer': 'retrain',
+
+        'num_trainable_params': None,
+        'train_time': None,
+        'val_losses': [],
+        'val_accs': [],
+        'test_loss': None,
+        'test_acc': None,
+        'confusion_matrix': None
+    }
+    model_infos = [base_model_info]
+
+    model_info = copy.deepcopy(base_model_info)
+    model_info['medical_mode'] = 'SALE'
+    model_infos.append(model_info)
+
+    model_info = copy.deepcopy(base_model_info)
+    model_info['medical_mode'] = 'LA_2C'
+    model_infos.append(model_info)
+
+    model_info = copy.deepcopy(base_model_info)
+    model_info['medical_mode'] = 'LA_3C'
+    model_infos.append(model_info)
+
+    model_info = copy.deepcopy(base_model_info)
+    model_info['medical_mode'] = 'LA_4C'
+    model_infos.append(model_info)
+
+    model_info = copy.deepcopy(base_model_info)
+    model_info['medical_mode'] = 'LALE'
+    model_infos.append(model_info)
+
+
+    model_info = copy.deepcopy(base_model_info)
+    model_info['name'] = 'VGG'
+    model_info['year'] = 2014
+    model_infos.append(model_info)
+
+    model_info = copy.deepcopy(base_model_info)
+    model_info['name'] = 'SqueezeNet'
+    model_info['year'] = 2016
+    model_infos.append(model_info)
+
+    model_info = copy.deepcopy(base_model_info)
+    model_info['name'] = 'MobileNet'
+    model_info['year'] = 2018
+    model_infos.append(model_info)
+    
+
+    model_info = copy.deepcopy(base_model_info)
+    model_info['num_classes'] = 2
+    model_infos.append(model_info)
+
+    model_info = copy.deepcopy(base_model_info)
+    model_info['num_classes'] = 4
+    model_infos.append(model_info)
+    
+
+    model_info = copy.deepcopy(base_model_info)
+    model_info['augmentation'] = 'min'
+    model_infos.append(model_info)
+
+    model_info = copy.deepcopy(base_model_info)
+    model_info['augmentation'] = 'max'
+    model_infos.append(model_info)
+    
+
+    model_info = copy.deepcopy(base_model_info)
+    model_info['transfer'] = 'finetune'
+    model_infos.append(model_info)
+
+    model_info = copy.deepcopy(base_model_info)
+    model_info['transfer'] = 'transfer'
+    model_infos.append(model_info)
+
+    for model, model_info in zip(model_objects, model_infos):
+        train_model(
+            model=model,
+            model_info=model_info,
+            epochs=epochs,
+            batch_size=batch_size,
+            gpu_index=gpu_index,
+            seed=seed
+        )
